e84c4cd5349798219ff0ce146d947852ada0f414
35427 <141239826+35427@users.noreply.github.com>
2023-10-12 16:18:58 +0900

    4-1

--- main.c
+++ main.c
@@ -3,42 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a, int b){
-	int result;
-	result = a+b;
-	return result;
-}
-
-int square(int n){
-	return n*n;
-}
-
-int get_max(int x, int y){
-	if (x>y){
-		return x;
-	}
-	else{
-		return y;
-	}
+void square(int a){
+	a = a*a;
+	
 }
 
 int main(){
-	
-	int a, b, n, x, y;
-	
-	a = 3;
-	b = 10;
-	n = 4;
-	x = 8;
-	y = 9;
-	
-	printf("sumTwo result : %d\n", sumTwo(a, b));
-	printf("square result : %d\n", square(n));
-	printf("get_max result : %d\n", get_max(x, y));
-	
-	
-	
-	return 0;
-	
-	
+	int a = 2;
+	square(a);
+	printf("a=%i\n",a);
+
 }
+
+
